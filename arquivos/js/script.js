b7673b0b12236b83461584c9c4fc067e55729a9c
Kauê Matheus <kauematheus7@hotmail.com>
2024-03-02 08:20:23 -0300

    Update script.js

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,1 +1,18 @@
-]~/
+const newImg = document.getElementById("newImg");
+const inputFile = document.querySelector("input[type=file]");
+const img = document.querySelector(".img-content img");
+const ButtonFilter = document.querySelectorAll(".filters-content button");
+const range = document.querySelector("input[type-range]");
+const spnRangeValue = document.getElementById("spnRangeValue");
+const btnResetFilters = document.getElementById("btnResetFilters");
+const btnSalvar= document.getElementById("btnSalvar");
+
+let rotate;
+let flipY;
+let flipX;
+
+let filterActive;
+
+let filters;
+
+
